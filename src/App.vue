ba628dc4402672dfbb78183a7d5bde5913050ba2
Aarón RJ <aaron@quantumstudio.es>
2020-01-15 14:18:55 +0100

    Commit #2 15/01/2020
    - Generado/creado el componente Picture.

--- src/App.vue
+++ src/App.vue
@@ -2,18 +2,21 @@
   <div id="app">
     <app-header></app-header>
     <app-score :wins="5" :defeats="10"></app-score>
+    <app-picture></app-picture>
   </div>
 </template>
 
 <script>
 import Header from '@/components/Header';
 import Score from '@/components/Score';
+import Picture from '@/components/Picture';
 
 export default {
   name: 'app',
   components: {
     'app-header': Header,
-    'app-score': Score
+    'app-score': Score,
+    'app-picture': Picture
   }
 }
 </script>
---
+++ src/components/Picture.vue
@@ -0,0 +1,27 @@
+<template lang="html">
+    <div class="picture">
+        Dibujo
+    </div>
+</template>
+
+<script lang="js">
+export default {
+    name: 'picture',
+    data() {
+        return {
+
+        }
+    }
+}
+</script>
+
+<style lang="scss">
+@import '../styles/_variables.scss';
+
+.picture {
+    width: 100%;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+}
+</style>
