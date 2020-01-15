bd53e0f54cd563d794fa155156f81db1a65c67b2
Aarón RJ <aresinodeveloper@gmail.com>
2020-01-15 22:24:09 +0100

    Commit #6 15/01/2020
    - Añadido html al Word component. Pinta las raya dependiendo de la longitud de la palabra a adivinar.

--- src/components/Word.vue
+++ src/components/Word.vue
@@ -1,16 +1,55 @@
 <template lang="html">
-    <div class="word">Word component</div>
+    <div class="word-container">
+        <div class="word-container-content">
+            <span :id="'letter-' + key" class="line"
+                  v-for="(item, key) in wordToArray"
+                  :key="key">_</span>
+        </div>
+    </div>
 </template>
 
 <script lang="js">
 export default {
     name: "word",
+    props: {
+        word: {
+            type: String,
+            required: true
+        }
+    },
     data() {
         return {}
-    }
+    },
+    computed: {
+        wordToArray: function () {
+            return Array.from(this.word);
+        }
+    },
 }
 </script>
 
 <style lang="scss">
 @import "../styles/_variables.scss";
+
+.word-container {
+    width: 100%;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    margin-top: 1em;
+
+    &-content {
+        width: 90%;
+        height: 60px;
+        border: 1px solid get-color("black");
+        display: flex;
+        justify-content: space-around;
+        align-items: center;
+
+        .line {
+            font-weight: bold;
+            font-size: 2.5em;
+        }
+    }
+}
 </style>
