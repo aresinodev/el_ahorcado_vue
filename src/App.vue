0aa7ba211df26c68f24726eabf9afb87ba122bad
Aarón RJ <aaron@quantumstudio.es>
2020-01-21 08:48:25 +0100

    Commit #1 21/01/2020
    - Cada vez que el usuario tiene un error, se pinta una parte del muñeco en el canvas. Si tiene 9 errores, lanza el popup de partida perdida.

--- src/App.vue
+++ src/App.vue
@@ -1,89 +1,86 @@
 <template>
-  <div>
-    <app-popup v-if="showPopup" :title="title" :subtitle="subtitle"
-               :error="error"></app-popup>
+	<div>
+		<app-popup v-if="showPopup" :title="title" :subtitle="subtitle" :error="error"></app-popup>
 
-    <div id="app">
-      <app-header></app-header>
-      <app-score :wins="wins" :defeats="defeats"></app-score>
-      <app-picture></app-picture>
-      <app-word :word="'coche'" :letterToCheck="letter"
-                @letterError="letterError" @rightWord="rightWord"></app-word>
-      <app-keyboard @letterPressed="letterPressed"
-                    :letterToPaint="letterToPaint"></app-keyboard>
-    </div>
-  </div>
+		<div id="app">
+			<app-header></app-header>
+			<app-score :wins="wins" :defeats="defeats"></app-score>
+			<app-picture :error="errors"></app-picture>
+			<app-word :word="'coche'" :letterToCheck="letter" @letterError="letterError" @rightWord="rightWord"></app-word>
+			<app-keyboard @letterPressed="letterPressed" :letterToPaint="letterToPaint"></app-keyboard>
+		</div>
+	</div>
 </template>
 
 <script>
-import Header from '@/components/Header';
-import Score from '@/components/Score';
-import Picture from '@/components/Picture';
+import Header from "@/components/Header";
+import Score from "@/components/Score";
+import Picture from "@/components/Picture";
 import Word from "@/components/Word";
 import Keyboard from "@/components/Keyboard";
-import InfoPopup from '@/components/InfoPopup';
+import InfoPopup from "@/components/InfoPopup";
 
 export default {
-  name: "app",
-  components: {
-    "app-header": Header,
-    "app-score": Score,
-    "app-picture": Picture,
-    "app-word": Word,
-    "app-keyboard": Keyboard,
-    "app-popup": InfoPopup
-  },
-  data() {
-    return {
-      letter: '',
-      letterToPaint: '',
-      wins: 0,
-      defeats: 0,
-      errors: 0,
-      showPopup: false,
-      error: false,
-      title: '',
-      subtitle: '',
-      wordOfMouth: 'coche'
-    }
-  },
-  methods: {
-    letterPressed: function (value) {
-      this.letter = value;
-    },
-    letterError: function (value) {
-      this.errors += 1;
-      console.log(this.errors);
+	name: "app",
+	components: {
+		"app-header": Header,
+		"app-score": Score,
+		"app-picture": Picture,
+		"app-word": Word,
+		"app-keyboard": Keyboard,
+		"app-popup": InfoPopup
+	},
+	data() {
+		return {
+			letter: "",
+			letterToPaint: "",
+			wins: 0,
+			defeats: 0,
+			errors: 0,
+			showPopup: false,
+			error: false,
+			title: "",
+			subtitle: "",
+			wordOfMouth: "coche",
+			errorNumber: 0
+		};
+	},
+	methods: {
+		letterPressed: function(value) {
+			this.letter = value;
+		},
+		letterError: function(value) {
+			this.errors += 1;
+			console.log(this.errors);
 
-      if (this.errors < 9) {
-        // TODO: Avisar que pinte parte del muñeco.
-        this.letterToPaint = value;
-      } else {
-        this.title = this.$t('infoPopup.errorTitle');
-        this.subtitle = this.$t('infoPopup.errorSubtitle', [this.wordOfMouth]);
-        this.error = true;
-        this.defeats += 1;
-        this.showPopup = true;
-      }
-    },
-    rightWord: function () {
-      this.title = this.$t('infoPopup.correctTitle');
-      this.subtitle = this.$t('infoPopup.correctSubtitle');
-      this.error = false;
-      this.wins += 1;
-      this.showPopup = true;
-    }
-  }
-}
+			if (this.errors < 9) {
+				this.letterToPaint = value;
+			} else {
+				this.title = this.$t("infoPopup.errorTitle");
+				this.subtitle = this.$t("infoPopup.errorSubtitle", [this.wordOfMouth]);
+				this.error = true;
+				this.defeats += 1;
+				this.showPopup = true;
+			}
+		},
+		rightWord: function() {
+			this.title = this.$t("infoPopup.correctTitle");
+			this.subtitle = this.$t("infoPopup.correctSubtitle");
+			this.error = false;
+			this.wins += 1;
+			this.showPopup = true;
+		}
+	}
+};
 </script>
 
 <style lang="scss" scoped>
 @import "./styles/_variables.scss";
 
 #app {
-  width: 100vw;
-  height: 100vh;
-  background-color: get-color("white");
-  margin: 0 auto;
+	width: 100vw;
+	height: 100vh;
+	background-color: get-color("white");
+	margin: 0 auto;
 }
 </style>
--- src/components/Picture.vue
+++ src/components/Picture.vue
@@ -1,87 +1,114 @@
 <template lang="html">
-    <div class="picture">
-        <canvas id="canvas"></canvas>
-    </div>
+  <div class="picture">
+    <canvas id="canvas"></canvas>
+  </div>
 </template>
 
 <script lang="js">
 export default {
-    name: 'app-picture',
+	name: 'app-picture',
+    props: {
+			error: {
+					type: Number
+			}
+    },
     data() {
-        return {
-
-        }
+			return {}
     },
     methods: {
-        drawDoll: function (piece) {
-            let canvas = document.getElementById("canvas");
-            let ctx = canvas.getContext("2d");
+			drawDoll: function (piece) {
+				let canvas = document.getElementById("canvas");
+				let ctx = canvas.getContext("2d");
 
-            switch (piece) {
-                case 1:
-                    ctx.beginPath();
-                    ctx.moveTo(0, 10);
-                    ctx.lineTo(30, 10);
-                    ctx.lineWidth = 3;
-                    ctx.stroke();
+				switch (piece) {
+					case 1:
+						// Línea 1
+						ctx.beginPath();
+						ctx.moveTo(0, 10);
+						ctx.lineTo(30, 10);
+						ctx.lineWidth = 2;
+						ctx.stroke();
 
-                    break;
+						break;
 
-                case 2:
-                    ctx.beginPath();
-                    ctx.moveTo(30, 9);
-                    ctx.lineTo(30, 25);
-                    ctx.lineWidth = 3;
-                    ctx.stroke();
+					case 2:
+						// Línea 2
+						ctx.beginPath();
+						ctx.moveTo(30, 9);
+						ctx.lineTo(30, 25);
+						ctx.lineWidth = 2;
+						ctx.stroke();
 
-                    break;
+						break;
 
-                case 3:
-                    ctx.beginPath();
-                    ctx.fillStyle = "transparent";
-                    ctx.lineWidth = 3;
-                    ctx.arc(30,34,8,0,2*Math.PI);
-                    ctx.fill();
-                    ctx.stroke();
+					case 3:
+						// Cabeza
+						ctx.beginPath();
+						ctx.fillStyle = "transparent";
+						ctx.lineWidth = 2;
+						ctx.arc(30,34,7,0,2*Math.PI);
+						ctx.fill();
+						ctx.stroke();
 
-                    break;
+						break;
 
-                case 4:
-                    ctx.beginPath();
-                    ctx.moveTo(30, 40);
-                    ctx.lineTo(30, 80);
-                    ctx.lineWidth = 3;
-                    ctx.stroke();
+					case 4:
+						// Tronco
+						ctx.beginPath();
+						ctx.moveTo(30, 40);
+						ctx.lineTo(30, 80);
+						ctx.lineWidth = 2;
+						ctx.stroke();
 
-                    break;
+						break;
 
-                case 5:
+					case 5:
+						// Brazo izquierdo
+						ctx.beginPath();
+						ctx.moveTo(30, 50);
+						ctx.lineTo(16, 60);
+						ctx.lineWidth = 2;
+						ctx.stroke();
 
-                    break;
+						break;
 
-                case 6:
-                    ctx.beginPath();
-                    ctx.moveTo(30, 50);
-                    ctx.lineTo(45, 60);
-                    ctx.lineWidth = 3;
-                    ctx.stroke();
+					case 6:
+						// Brazo derecha
+						ctx.beginPath();
+						ctx.moveTo(30, 50);
+						ctx.lineTo(45, 60);
+						ctx.lineWidth = 2;
+						ctx.stroke();
 
-                    break;
+						break;
 
-                case 7:
+					case 7:
+						// Pierna izquierda
+						ctx.beginPath();
+						ctx.moveTo(30, 79);
+						ctx.lineTo(20, 100);
+						ctx.lineWidth = 2;
+						ctx.stroke();
 
-                    break;
+						break;
 
-                case 8:
+					case 8:
+						// Pierna derecha
+						ctx.beginPath();
+						ctx.moveTo(30, 79);
+						ctx.lineTo(40, 100);
+						ctx.lineWidth = 2;
+						ctx.stroke();
 
-                    break;
-
-                case 9:
-
-                    break;
-            }
-        }
-    },
+						break;
+				}
+			}
+		},
+		watch: {
+			error: function(value) {
+				this.drawDoll(value);
+			}
+		},
 }
 </script>
 
@@ -89,15 +116,15 @@ export default {
 @import '../styles/_variables.scss';
 
 .picture {
-    width: 100%;
-    display: flex;
-    justify-content: center;
-    align-items: center;
+	width: 100%;
+	display: flex;
+	justify-content: center;
+	align-items: center;
 
-    canvas {
-        width: 50%;
-        height: 300px;
-        border: 1px solid get-color('black');
-    }
+	canvas {
+			width: 300px;
+			height: 150px;
+			border: 1px solid get-color('black');
+	}
 }
 </style>
